866a02284e797a851b36b5b1666034f2b45de56a
Gabriela Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-10-05 20:53:32 +0200

    [FastFall] v1 I think?

---
+++ code/Race/Kart/FastFall.cpp
@@ -0,0 +1,21 @@
+#include <kamek.hpp>
+#include <game/Race/Kart/KartStatus.hpp>
+#include <game/Race/Kart/KartHolder.hpp>
+
+void FastFallCalc(KartStatus * status, KartPhysics * physics)
+{
+    if(status->airtime >= 2)
+    {
+        if(!status->bool_0x96 || (status->airtime >= 20))
+        {
+            ControllerHolder * holder = status->base->GetControllerHolder();
+            float y_input = holder->inputStates[0].stickY;
+            if(y_input <= 0.0f) y_input = 0.0f;
+            // else y_input *= 2;
+            physics->gravity -= y_input * 0.39f;
+        }
+    }
+    status->UpdateFromInput();
+}
+
+kmCall(0x805967a4, FastFallCalc);
