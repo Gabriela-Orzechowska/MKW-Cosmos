41432fee9411daeec4d911f4f6db7f27971ee6b3
Gabriela Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-10-12 12:21:45 +0200

    [FileManager] Create FileManager

---
+++ code/FileManager/FileManager.cpp
@@ -0,0 +1,117 @@
+#include <FileManager/FileManager.hpp>
+#include <main.hpp>
+
+namespace DXFile
+{
+    FileManager * FileManager::sInstance = NULL;
+
+    void FileManager::CreateStaticInstance()
+    {
+        FileManager * manager;
+        bool valid = false;
+        EGG::Heap * heap = RKSystem::mInstance.EGGSystem;
+        s32 ret = DX::Open("file", IOS::MODE_NONE); //Check if its riivo
+        if(ret < 0) //If not, check if dolphin
+        {
+            ret = DX::Open("/dev/dolphin", IOS::MODE_NONE);
+            if(ret >= 0)
+            {
+                valid = true;
+                IOS::Close(ret);
+            }
+            manager = new (heap) FileManager();
+        }
+            //We might need a riivo helper
+        FileManager::sInstance = manager;
+        manager->isValid = valid;
+        FileManager::sInstance->taskThread = EGG::TaskThread::Create(2, 30, 0x2000, NULL);
+    }
+
+    BootHook SetUpFileLoader(FileManager::CreateStaticInstance, FIRST);
+
+    FileManager * FileManager::GetStaticInstance()
+    {
+        return FileManager::sInstance;
+    }
+
+    void FileManager::GetCorrectPath(char * realPath, const char * path) const
+    {
+        snprintf(realPath, IPCMAXPATH, "%s%s", "/title/00010004", path);
+        return;
+    }
+
+    s32 FileManager::GetDeviceFd() const{
+    return IOS::fs_fd;
+    }
+
+    s32 FileManager::Open(const char * path, u32 mode)
+    {
+        char realPath[IPCMAXPATH];
+        this->GetCorrectPath(realPath, path);
+        strcpy(this->path, realPath);
+
+        this->fd = DX::Open(realPath, (IOS::Mode) mode);
+        this->fileSize = IOS::Seek(this->fd, 0, IOS::SEEK_END);
+
+        IOS::Seek(this->fd, 0, IOS::SEEK_START);
+        return this->fd;
+    }
+
+    s32 FileManager::CreateOpen(const char * path, u32 mode)
+    {
+        char realPath[IPCMAXPATH];
+        this->GetCorrectPath(realPath, path);
+        ISFS::CreateFile(realPath, 0, IOS::MODE_READ_WRITE, IOS::MODE_READ_WRITE, IOS::MODE_READ_WRITE);
+        return this->Open(path, mode);
+    }
+
+    s32 FileManager::Read(void * buffer, s32 size)
+    {
+        if(this->fd < 0) return -1;
+        return IOS::Read(this->fd, buffer, size);
+    }
+
+    s32 FileManager::Write(u32 size, void *buffer){
+        if(this->fd < 0) return -1;
+        return IOS::Write(this->fd, buffer, size);
+    }
+
+    s32 FileManager::Overwrite(u32 size, void *buffer){
+        if(this->fd < 0) return -1;
+        IOS::Seek(this->fd, 0, IOS::SEEK_START);
+        return IOS::Write(this->fd, buffer, size);
+    }
+
+    void FileManager::Close(){
+        if(this->fd >=0) IOS::Close(this->fd);
+        this->fd = -1;
+        this->fileSize = -1;
+    }
+
+    s32 RiivoFileManager::Open(const char * filepath, u32 mode){
+        return FileManager::Open(path, this->GetRiivoMode(mode));
+    }
+
+    s32 RiivoFileManager::CreateOpen(const char * filepath, u32 mode){
+        s32 riivo_fd = this->GetDeviceFd();
+        IOS::IOCtl(riivo_fd, (IOS::IOCtlType) RIIVO_IOCTL_CREATEFILE, (void*) filepath, strlen(filepath)+1, NULL, 0);
+        IOS::Close(riivo_fd);
+        return this->Open(filepath, mode);
+    }
+
+    s32 RiivoFileManager::GetDeviceFd() const{
+        return DX::Open("file", IOS::MODE_NONE);
+    }
+
+    void RiivoFileManager::GetCorrectPath(char * realPath, const char * path) const
+    {
+        snprintf(realPath, IPCMAXPATH, "%s%s", "file", path);
+        return;
+    }
+
+    RiivoMode RiivoFileManager::GetRiivoMode(u32 mode) const{
+        if((mode & 0x3) != 0) mode -= 1;
+        return (RiivoMode) mode;
+    }
+
+}
---
+++ code/FileManager/FileManager.hpp
@@ -0,0 +1,63 @@
+#ifndef _DX_FILE_MANAGER_
+#define _DX_FILE_MANAGER_
+
+#include <FileManager/Riivolution.hpp>
+#include <kamek.hpp> 
+#include <core/egg/Heap.hpp>
+#include <core/egg/Thread.hpp>
+#include <core/System/RKSystem.hpp>
+#include <core/rvl/ipc/ipc.hpp>
+#include <core/rvl/devfs/isfs.hpp>
+
+typedef char FileName[255];
+
+namespace DXFile
+{
+    enum FILE_MODE{
+        FILE_MODE_NONE,
+        FILE_MODE_READ,
+        FILE_MODE_WRITE,
+        FILE_MODE_READ_WRITE = FILE_MODE_READ | FILE_MODE_WRITE,
+    };
+
+    class FileManager{
+        protected:
+            s32 fd;
+            s32 fileSize;
+            char path[IPCMAXPATH];
+            bool isValid;
+
+        public:
+            static FileManager * sInstance;
+            static void CreateStaticInstance();
+            static FileManager * GetStaticInstance();
+            FileManager() : fd(-1) {memset(&this->path, 0, IPCMAXPATH);}
+            virtual s32 Open(const char * filepath, u32 mode);
+            virtual s32 CreateOpen(const char * filepath, u32 mode);
+            virtual s32 GetDeviceFd() const;
+            virtual void GetCorrectPath(char * realPath, const char * path) const;
+            s32 Read(void * buffer = NULL, s32 size = 0);
+            s32 Write(u32 size, void * buffer);
+            s32 Overwrite(u32 size, void * buffer);
+            void Close();
+            int GetFileSize() const;
+            EGG::TaskThread * taskThread;
+            static const char * modPath;
+    };
+
+    bool isRiivo();
+
+    class RiivoFileManager : public FileManager{
+        public:
+            RiivoFileManager(){};
+            s32 Open(const char * filepath, u32 mode) override;
+            s32 CreateOpen(const char * filepath, u32 mode) override;
+            s32 GetDeviceFd() const override;
+        private:
+            void GetCorrectPath(char *realPath, const char *path) const override;
+            RiivoMode GetRiivoMode(u32 mode) const;
+    };
+
+}
+
+#endif
