f6d790e650db828d378f762e9793995c50baf5eb
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-10-15 13:12:15 +0200

    [ExceptionHandler] Remove custom handler

--- code/Debug/ExceptionHandler.cpp
+++ code/Debug/ExceptionHandler.cpp
@@ -11,17 +11,10 @@
 
 namespace DXDebug{
 
-    //Credit Star and Riidefi
-    kmWrite16(0x802A7410, 0x00000023);
-    kmWrite32(0x802A7404, 0x00000000);
-    kmWrite32(0x80023948, 0x281e0007);
-    kmWrite32(0x80009324, 0x38800068);
-
     void SetConsoleParams(){
         nw4r::db::detail::ConsoleHead *console = EGG::Exception::console;
         console->viewLines = 0x16;
         console->viewPosY = 0x08;
-        console->viewPosX = 0x08;
     }
     BootHook ConsoleParams(SetConsoleParams, LOW);
 
