11c3e69c6bc66e49e68200b50b6adc3a3306f8dc
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-10-29 22:26:18 +0100

    [GhostManager] Load Custom Ghost Files

--- code/Ghost/GhostManager.cpp
+++ code/Ghost/GhostManager.cpp
@@ -9,3 +9,140 @@ void CorrectGhostTrackName(LayoutUIControl * control, const char *textBoxName, u
 }
 
 kmCall(0x805e2a4c, CorrectGhostTrackName);
+
+namespace DXGhost
+{
+    GhostManager * GhostManager::sInstance = NULL;
+    char GhostManager::folderPath[IPCMAXPATH] = "";
+
+    GhostManager::GhostManager() : courseId(-1)
+    {
+        this->folderManager = DXFile::FolderManager::Create();
+        this->files = NULL;
+    }
+
+    GhostManager::~GhostManager() 
+    {
+        delete(this->folderManager);
+    }
+
+    GhostManager * GhostManager::CreateStaticInstance()
+    {
+        GhostManager * holder = GhostManager::sInstance;
+        if(holder == nullptr)
+        {
+            holder = new(RKSystem::mInstance.EGGSystem) GhostManager;
+            GhostManager::sInstance = holder;
+        }
+        holder->Reset();
+        return holder;
+    }
+
+    void GhostManager::DestroyStaticInstance()
+    {
+        if(GhostManager::sInstance != nullptr) delete(GhostManager::sInstance);
+        GhostManager::sInstance = nullptr;
+    }
+
+    const char * ttFolders[2] = { "150", "200", };
+
+    void GhostManager::Init(u32 courseId)
+    {
+        this->Reset();
+        this->courseId = courseId;
+        DXFile::FolderManager * manager = this->folderManager;
+
+        snprintf(folderPath, IPCMAXPATH, "%s/%03x", DX::ghostFolder, courseId);
+        DXFile::FileManager::sInstance->CreateFolder(folderPath);
+
+        char folderModePath[IPCMAXPATH];
+        snprintf(folderModePath, IPCMAXPATH, "%s/%s", folderPath, ttFolders[DX::TTMode]);
+        DXFile::FileManager::sInstance->CreateFolder(folderModePath);
+
+        manager->ReadFolder(folderModePath);
+
+        this->files = new (RKSystem::mInstance.EGGSystem) GhostData[manager->GetFileCount()];
+        RKG * rkg = &this->rkg;
+        u32 counter = 0;
+        for(int i = 0; i < manager->GetFileCount(); i++)
+        {
+            rkg->ClearBuffer();
+            GhostData * header = &this->files[counter];
+            //header->isValid = false;
+            if(manager->ReadFile(rkg, i, DXFile::FILE_MODE_READ) > 0 && rkg->CheckValidity())
+            {
+                header->Init(rkg);
+                counter++;
+            }
+            manager->CloseFile();
+        }
+        this->rkgCount = counter;
+
+    }
+
+    void GhostManager::Reset()
+    {
+        this->courseId = -1;
+        mainGhostIndex = 0xFF;
+        delete[] this->files;
+        this->files = nullptr;
+        RaceData * raceData = RaceData::sInstance;
+        raceData->menusScenario.players[1].playerType = PLAYER_NONE;
+    }
+
+    bool GhostManager::LoadGhost(RKG * rkg, u32 index)
+    {
+        rkg->ClearBuffer();
+        this->folderManager->ReadFile(rkg, index, DXFile::FILE_MODE_READ);
+        return rkg->CheckValidity();
+    }
+
+    void GhostManager::CreateAndSaveFiles(void * holder)
+    {
+        char path[IPCMAXPATH];
+        GhostManager * manager = (GhostManager *) holder;
+        RKG * rkg = &manager->rkg;
+        snprintf(path, IPCMAXPATH, "%s/%01dm%02ds%03d.rkg", manager->folderManager->GetName(), rkg->header.minutes, rkg->header.seconds, rkg->header.milliseconds);
+        DXFile::FileManager * fileManager = DXFile::FileManager::sInstance;
+        fileManager->CreateOpen(path, DXFile::FILE_MODE_WRITE);
+        u32 size = sizeof(RKG);
+        if(rkg->header.compressed) size = ((CompressedRKG*)rkg)->dataLength + sizeof(RKGHeader) + 0x4 + 0x4;
+        fileManager->Overwrite(size, rkg);
+        fileManager->Close();
+    }
+
+    void CustomGhostGroup(GhostList * list, u32 id)
+    {
+        u32 trackID = LeCode::LeCodeManager::GetStaticInstance()->GetTrackID();
+        GhostManager * manager = GhostManager::GetStaticInstance();
+        manager->Init(trackID);
+        u32 index = 0;
+        for(int i = 0; i < manager->folderManager->GetFileCount(); i++)
+        {
+            if(index == 38) break;
+            const GhostData * data = manager->GetGhostData(i);
+            if(data->isValid)
+            {
+                list->entries[index].data = data;
+                index++;
+            }
+        }
+        list->count = index;
+        qsort(list, list->count, sizeof(GhostListEntry), (int (*)(const void *, const void *))*&GhostList::CompareEntries);
+    };
+    kmCall(0x806394f0, CustomGhostGroup);
+
+    void * CreatePageAndManager()
+    {
+        GhostManager::CreateStaticInstance();
+        return new(Pages::GhostSelect);
+    }
+    kmCall(0x80623dec, CreatePageAndManager);
+    kmWrite32(0x80623de0, 0x60000000);
+
+    void DestroyManager(Scene *scene, PageId pageId){
+        scene->CreatePage(pageId);
+        GhostManager::DestroyStaticInstance();
+    }
+    kmCall(0x8062cf98, DestroyManager);
+}
--- code/Ghost/GhostManager.hpp
+++ code/Ghost/GhostManager.hpp
@@ -2,7 +2,46 @@
 #define _DX_GHOST_MANAGER_
 
 #include <kamek.hpp>
+#include <main.hpp>
 #include <game/UI/Ctrl/UIControl.hpp>
+#include <game/UI/Page/Other/GhostManager.hpp>
+#include <FileManager/FileManager.hpp>
+#include <FileManager/FolderManager.hpp>
 #include <LeCode/LeCodeManager.hpp>
+#include <game/UI/Page/Other/TTSplits.hpp>
+#include <game/Race/RaceData.hpp>
+#include <game/UI/Page/Other/GhostSelect.hpp>
+
+namespace DXGhost
+{
+    class GhostManager{
+        public:
+            GhostManager();
+            ~GhostManager();
+            static GhostManager * GetStaticInstance() { return sInstance; }
+            static GhostManager * CreateStaticInstance();
+            static void DestroyStaticInstance();
+            void Reset();
+            void Init(u32 courseId);
+            const GhostData * GetGhostData(u32 idx) const {return &this->files[idx];}
+            bool LoadGhost(RKG * rkg, u32 index);
+            //void LoadAllGhosts(RKG * rkg, u32 maxCount, bool isGhostRace);
+            static void CreateAndSaveFiles(void * holder);
+
+            RKG rkg  __attribute__ ((aligned( 0x20 )));
+            u32 mainGhostIndex;
+            static char folderPath[IPCMAXPATH];
+            DXFile::FolderManager * folderManager;
+
+        private:
+            static GhostManager * sInstance;
+            GhostData * files;
+            u32 courseId;
+            u32 rkgCount;
+            u32 padding[20];
+    };
+}
+
+
 
 #endif
