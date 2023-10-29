b6c8808d182bebf87e1005adcb00a9037e3ff512
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-10-29 22:34:35 +0100

    [FileManager] Change NAND Path

--- Build.bat
+++ Build.bat
@@ -28,7 +28,7 @@ echo Linking...
 "KamekSource/bin/Debug/net6.0/Kamek" "build/kamek.o" %OBJECTS% -dynamic -externals="source/symbols.txt" -versions="source/versions.txt" -output-kamek=build\$KV$.bin
 
 if %ErrorLevel% equ 0 (
-    xcopy /Y build\*.bin ".\TestPack\mkwdx\sys"
+    xcopy /Y build\*.bin ".\TestPack\DX\sys"
 	del build\*.o
 	del build\*.bin
 
