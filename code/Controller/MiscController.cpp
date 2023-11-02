6eae18fbf52c97b65e54c38d01c94a064335b69d
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2023-11-02 21:43:23 +0100

    [MiscCont] Way more sophisticated Y to Wheelie

--- code/Controller/MiscController.cpp
+++ code/Controller/MiscController.cpp
@@ -120,23 +120,5 @@ namespace DXController
     }
 }
 
-#ifndef __INTELLISENSE__
-// Y to Wheelie
-kmBranchDefAsm(0x801afbf8, 0x801afbfc)
-{
-    nofralloc
-
-    li r14, 0x880
-    and r15, r14, r0
-    cmpw r15, r14
-    bne+ end
-    sub r0, r0, r15
-    li r15, 0x88
-    add r0, r0, r15
-
-    end:
-    sth r0, 0(r4)
-
-    blr
-}
-#endif
+kmWrite32(0x80520318,0x71240808);
+kmWrite32(0x8051f670,0x70e40081);
