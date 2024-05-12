6d62a09a41316d11bd228950ad0792af9acfb3eb
Gabriela-Orzechowska <gabriela.orzechowska.2889@gmail.com>
2024-05-12 18:49:51 +0200

    [Scene] Speedup Dolphin while Loading

---
+++ code/UI/Scene.cpp
@@ -0,0 +1,19 @@
+#include <kamek.hpp>
+#include <Debug/IOSDolphin.hpp>
+#include <core/egg/Fader.hpp>
+
+namespace Cosmos
+{
+    u32 SetDolphinSpeed(EGG::ColorFader* fader, u32, u32, u32 ret){
+        if(IOS::Dolphin::IsOpen())
+        {
+            if(fader->status == EGG::Fader::CAN_FADE_IN)  
+                IOS::Dolphin::SetSpeedLimit(0);
+            else
+                IOS::Dolphin::SetSpeedLimit(100);
+        }
+
+        return ret;
+    }
+    kmBranch(0x802153b0, SetDolphinSpeed);
+} // namespace Cosmos
