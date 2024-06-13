8bf4dc769abf807940b3bca023595ab4b3529672
Gabriela Orzechowska <gabriela.orzechowska.2889@gmail.com>
2024-06-13 23:03:20 +0200

    [GhostReplay] Correct transparency

--- code/Ghost/GhostReplay.cpp
+++ code/Ghost/GhostReplay.cpp
@@ -1,30 +1,46 @@
+#include <kamek.hpp>
 #include <game/System/identifiers.hpp>
 #include <game/UI/MenuData/MenuData.hpp>
-#include <kamek.hpp>
 #include <game/Race/RaceData.hpp>
 #include <game/UI/Page/RaceHUD/TTInterface.hpp>
 #include <game/UI/Page/RaceHUD/RaceHUD.hpp>
+#include <game/Race/Kart/Kart.hpp>
 
-int MakePlayerLocal(RacedataPlayer& player, int id){
+int MakePlayerLocal(RacedataPlayer& player){
+    register u32 id;
+    asm { ASM(mr id, r0);}
     PlayerType type = RaceData::GetStaticInstance()->racesScenario.players[id].playerType;
-    if(id == 0 && type == PLAYER_GHOST) return PLAYER_REAL_LOCAL;
-    return type;
+    return id == 0 && type == PLAYER_GHOST ? PLAYER_REAL_LOCAL : type;
 }
-
 kmCall(0x80594444, MakePlayerLocal);
-kmWrite32(0x80594434, 0x889F0010);
-kmWrite32(0x80594448, 0x2c030000);
-kmWrite32(0x80594450, 0x2c030001);
-kmWrite32(0x80594458, 0x2c030003);
 
+int FixTransparency(const Kart& kart){
+    u32 playerIdx = kart.values->playerIdx;
+    PlayerType type = RaceData::GetStaticInstance()->racesScenario.players[playerIdx].playerType;
+    return playerIdx == 0 && type == PLAYER_GHOST ? PLAYER_REAL_LOCAL : type;
+}
+kmCall(0x8058e264, FixTransparency);
+
+//Use r3 for comparing instead
+kmWrite16(0x80594448, 0x2c03);
+kmWrite16(0x80594450, 0x2c03);
+kmWrite16(0x80594458, 0x2c03);
+//Activate TT interface
 kmWrite32(0x80630450, 0x3880000d);
 kmWrite32(0x80630474, 0x3880000d);
 kmWrite32(0x80630498, 0x3880000d);
 
-PageId GetProperTTPausePage(Pages::RaceHUD& hud) {
+kmWrite16(0x805e1e7A, 0x0000);
+
+inline bool isWatchingGhost(){
     MenuId id = MenuData::GetStaticInstance()->GetCurrentScene()->menuId;
-    if(id >= WATCH_GHOST_FROM_CHANNEL && id <= WATCH_GHOST_FROM_MENU) return GHOST_REPLAY_PAUSE_MENU;
-    return hud.GetPausePageId();
+    return id >= WATCH_GHOST_FROM_CHANNEL && id <= WATCH_GHOST_FROM_MENU;
+}
+
+PageId GetProperTTPausePage(Pages::RaceHUD& hud) {
+    return isWatchingGhost() ? GHOST_REPLAY_PAUSE_MENU : hud.GetPausePageId();
 }
 kmCall(0x808569e0, GetProperTTPausePage);
+
+
 
