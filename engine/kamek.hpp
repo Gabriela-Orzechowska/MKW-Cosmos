0109cb61de656b7b8133e9658da9edb6aa39e625
Gabriela Orzechowska <gabriela.orzechowska.2889@gmail.com>
2024-08-28 13:54:04 +0200

    Update kamek.hpp

--- engine/kamek.hpp
+++ engine/kamek.hpp
@@ -117,7 +117,8 @@ struct PtmfHolder_3A : PtmfHolderBase_3A<Ret, A1, A2, A3> {
 #define CosmosLog(f, ...) OSReport("[Cosmos:%s:%d] " f, __FILE__, __LINE__, ##__VA_ARGS__)
 #define CosmosError(f, ...) OSReport("[Cosmos Error:%s:%d] " f, __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define COSMOS_ASSERT(c) if(!(c)) { OSReport("[Cosmos Assert Failed:%s:%d]" #c, __FILE__, __LINE__); u32 _blackColor = 0; u32 _whiteColor = ~0; OSFatal(&_whiteColor, &_blackColor, "Assertion Failed! " #c); }
+#define COSMOS_ASSERT(c) if(!(c)) { OSReport("[Cosmos Assert Failed:%s:%d]" #c, __FILE__, __LINE__); \
+    u32 _blackColor = 0; u32 _whiteColor = ~0; OSFatal(&_whiteColor, &_blackColor, "Assertion Failed! " #c); }
 #define COSMOS_ASSERT_NOT_NULL(c) COSMOS_ASSERT(c != nullptr);
 
 #define COSMOS_SECURITY
