69b89e4921021a24173731278b24efd7e7226c6f
Gabriela Orzechowska <gabriela.orzechowska.2889@gmail.com>
2025-01-17 21:13:53 +0100

    Destructor is not called during destruction

--- code/Settings/MiscSettings.cpp
+++ code/Settings/MiscSettings.cpp
@@ -103,11 +103,13 @@ void SaveMinimap(CtrlRace2DMap& map, const char* name){
 }
 kmCall(0x80858204, SaveMinimap);
 
+
+
 void* ResetMinimap(void* ptr){
     mainMinimap = nullptr;
     return ptr;
 };
-kmBranch(0x807ebedc, ResetMinimap);
+kmBranch(0x8051b370, ResetMinimap);
 
 void PatchMiiHeads() {
     if(mainMinimap == nullptr) return;
